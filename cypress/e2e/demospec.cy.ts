126dc16e666f1db25a82fa44d0c51dbb4be76e7e
Kiran Shigli <kshigli@gmail.com>
2024-01-12 18:27:55 +0000

    cypress_api_test

--- cypress/e2e/demospec.cy.ts
+++ cypress/e2e/demospec.cy.ts
@@ -5,7 +5,8 @@ describe('template spec', () => {
     cy.request({
       method: 'POST',
       url: "https://dummy.restapiexample.com/api/v1/create",
-      body: {"name":"TestUser1005","salary":"600001","age":"7881"}
+      body: {"name":"TestUser1005","salary":"600001","age":"7881"},
+      failOnStatusCode: false
     }).then(( response ) => {
       userid = response.body.data.id;
       cy.log('userid--'+userid);
